0578f0321c0f5ab65eea757ea4bb1cbe43afb625
Dazaev Vives <dazaev.vivez@accenture.com>
2018-10-09 23:28:51 -0500

    trying again god oh god

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
 import GamePage from './components/GamePage/GamePage';
+import WinPage from './components/WinPage/WinPage';
+import LostPage from './components/LostPage/LostPage';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Route exact={true} path={'/'} component={HomePage} />
+        <Route exact={true} path={'/game'} component={GamePage} />
+        <Route exact={true} path={'/winpage'} component={WinPage} />
+        <Route exact={true} path={'/lostpage'} component={LostPage} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 import HomePage from './components/HomePage/HomePage';
 import GamePage from './components/GamePage/GamePage';
 import WinPage from './components/WinPage/WinPage';
 import LostPage from './components/LostPage/LostPage';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Router>
-    <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/game" component={GamePage} />
-      <Route path="/winpage" component={WinPage} />
-      <Route path="/lostpage" component={LostPage} />
-    </div>
+    <App />
   </Router>,
   document.getElementById('root')
 );
